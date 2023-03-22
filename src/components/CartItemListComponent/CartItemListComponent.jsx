0c599f025322f125df5616d597545b13001d62aa
Erick Osuna Saavedra <erick_osuna@hotmail.com>
2023-03-22 15:53:18 -0300

    Agregando Estilos al ProductItemList y al CartItemList

--- src/components/CartItemListComponent/CartItemListComponent.jsx
+++ src/components/CartItemListComponent/CartItemListComponent.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
 import style from "./style.css"
+import { ButtonComponent } from "../ButtonComponent/ButtonComponent"
+import almacenamiento01 from "../../img/almacenamiento01.jpg"
 
 export const CartItemListComponent = () => {
   return (
-    <div className='cartItemList'>CartItemListComponent</div>
+    <div className='cartItemList'>
+      <p className='cartEmpty'>Tu carrito está vacío.</p>
+      <div className='cartProducts'>
+        <div className="cartProduct">
+          <img src={almacenamiento01} alt="" />
+          <div className="cartProducttitle">
+            <p>Título</p>
+            <h3>Almacenamiento 01</h3>
+          </div>
+          <div className="cartProductQuantity">
+            <p>Cantidad</p>
+            <p>1</p>
+          </div>
+          <div className="cartProductPrice">
+            <p>Precio</p>
+            <p>$ 1000</p>
+          </div>
+          <div className="cartProductSubTotal">
+            <p>Subtotal</p>
+            <p>$ 1000</p>
+          </div>
+          <button className='cartProductRemove'></button>
+          <ButtonComponent label='Eliminar' className='cartProductRemove'/>
+        </div>
+      </div>
+      <div className="cartActions">
+        <div className="cartLeftActions">
+          <ButtonComponent className='cartActionsRemove' label='Vaciar Carrito' />
+        </div>
+        <div className="cartRightActions">
+          <div className="cartActionsTotal">
+            <p>Total:</p>
+            <p id='total'>$ 3000</p>
+          </div>
+          <ButtonComponent className='cartActionsBuy' label='Comprar Ahora' />
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/ProductCardComponent/ProductCardComponent.jsx
+++ src/components/ProductCardComponent/ProductCardComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import "./style.css";
-import { ButtonComponent} from "../ButtonComponent/ButtonComponent";
+import { ButtonComponent } from "../ButtonComponent/ButtonComponent";
 
 export const ProductCardComponent = () => {
 
@@ -13,24 +13,26 @@ export const ProductCardComponent = () => {
       .then(json => setData(json))
   }
 
-  useEffect ( () => {
+  useEffect(() => {
     traerProductos()
     console.log(data);
   }, []);
 
 
   return (
-      <>
-        {
-          data.map((e)=>
+    <>
+      {
+        data.map((e) =>
           <div className="productCard" key={e.id}>
-            <img src={e.imagen} alt="" className='productImage'/>
-            <div className='productInfoName'>{e.titulo}</div>
-            <div className='productInfoPrice'>ARS $ {e.precio}</div>
-            <ButtonComponent label='Agregar'/>
+            <img src={e.imagen} alt="" className='productImage' />
+            <div className='productCardInfo'>
+              <div className='productInfoName'>{e.titulo}</div>
+              <div className='productInfoPrice'>ARS $ {e.precio}</div>
+              <ButtonComponent label='Agregar' />
+            </div>
           </div>
-          )
-        }
-      </>
+        )
+      }
+    </>
   )
 }
