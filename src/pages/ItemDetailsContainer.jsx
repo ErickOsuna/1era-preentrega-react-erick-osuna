f69987e2c33cb314d1a2486d3ee7767f20bb5306
Erick Osuna Saavedra <erick_osuna@hotmail.com>
2023-03-26 14:43:34 -0300

    Otro commit

--- src/pages/ItemDetailsContainer.jsx
+++ src/pages/ItemDetailsContainer.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { CategoryNavBarComponent } from '../components'
-import { ItemDetailsComponent } from '../components'
+import { products } from '../data/products'
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 export const ItemDetailsContainer = () => {
+
+  const { id } = useParams();
+  const [items] = useState(products);
+  const resultado = items.find(e => e.id === id);
+
   return (
     <div>
       <CategoryNavBarComponent />
-      <ItemDetailsComponent />
+      <div className='itemListContainer'>
+        <span>{resultado.titulo}</span>
+      </div>
     </div>
   )
 }
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ItemDetailsComponent } from '../components';
 import { Home, Productos, ArmaTuPc, Contacto, IniciaSesion, CartWidget, ItemDetailsContainer, ItemCategoryContainer } from "../pages";
 
 export const MainRoutes = () => {
@@ -9,7 +10,7 @@ export const MainRoutes = () => {
         <Route exact path="/" element={<Home />}/>
         <Route exact path="/category" element={<Productos />}/>
         <Route exact path="/category/:id" element={<ItemCategoryContainer />}/>
-        <Route exact path='/item/:id' element={<ItemDetailsContainer/>}/>
+        <Route exact path="/item/:id" element={<ItemDetailsComponent/>}/>
         <Route exact path="/arma-tu-pc" element={<ArmaTuPc />}/>
         <Route exact path="/contacto" element={<Contacto />}/>
         <Route exact path="/inicia-sesion" element={<IniciaSesion />}/>
